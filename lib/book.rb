a6d024694ec6b31d5e4027f9f989a8e262c48641
Patrick Liang <pl204511194@yahoo.com>
2022-02-23 10:51:15 -0500

    commit tree 8ae955c3e60d5c841ea897d1dffaa3b03463c559

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+some_book = Book.new("And Then There Were None")
+some_book.author = "Agatha Christie"
+some_book.page_count = 272
+some_book.genre = "Mystery"
+puts some_book.turn_page
